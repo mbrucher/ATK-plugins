1109c79641dca817ce889ce46cfc813da764ea4e
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-06-06 23:22:26 +0100

    Compiling test plugin

---
+++ ATKSD1/ATKSD1.h
@@ -0,0 +1,20 @@
+#ifndef __ATKSD1__
+#define __ATKSD1__
+
+#include "IPlug_include_in_plug_hdr.h"
+
+class ATKSD1 : public IPlug
+{
+public:
+  ATKSD1(IPlugInstanceInfo instanceInfo);
+  ~ATKSD1();
+
+  void Reset();
+  void OnParamChange(int paramIdx);
+  void ProcessDoubleReplacing(double** inputs, double** outputs, int nFrames);
+
+private:
+  double mGain;
+};
+
+#endif
