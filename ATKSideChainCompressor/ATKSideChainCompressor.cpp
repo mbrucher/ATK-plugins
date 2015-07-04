7b3b09c79f751715869b839898d62c8de2e45af5
Matthieu Brucher <matthieu.brucher@gmail.com>
2015-07-04 23:47:36 +0100

    Basic version, update to wdl-ol, so need to check that

---
+++ ATKSideChainCompressor/ATKSideChainCompressor.cpp
@@ -0,0 +1,172 @@
+#include <cmath>
+
+#include "ATKSideChainCompressor.h"
+#include "IPlug_include_in_plug_src.h"
+#include "IControl.h"
+#include "controls.h"
+#include "resource.h"
+
+const int kNumPrograms = 2;
+
+enum EParams
+{
+  kAttack = 0,
+  kRelease,
+  kThreshold,
+  kSlope,
+  kSoftness,
+  kMakeup,
+  kDryWet,
+  kNumParams
+};
+
+enum ELayout
+{
+  kWidth = GUI_WIDTH,
+  kHeight = GUI_HEIGHT,
+
+  kAttackX = 25,
+  kAttackY = 26,
+  kReleaseX = 94,
+  kReleaseY = 26,
+  kThresholdX = 163,
+  kThresholdY = 26,
+  kSlopeX = 232,
+  kSlopeY = 26,
+  kSoftnessX = 301,
+  kSoftnessY = 26,
+  kMakeupX = 370,
+  kMakeupY = 26,
+  kDryWetX = 439,
+  kDryWetY = 26,
+  kKnobFrames = 43
+};
+
+ATKSideChainCompressor::ATKSideChainCompressor(IPlugInstanceInfo instanceInfo)
+  :	IPLUG_CTOR(kNumParams, kNumPrograms, instanceInfo),
+    inFilter(NULL, 1, 0, false), outFilter(NULL, 1, 0, false)
+{
+  TRACE;
+
+  //arguments are: name, defaultVal, minVal, maxVal, step, label
+  GetParam(kAttack)->InitDouble("Attack", 10., 1., 100.0, 0.1, "ms");
+  GetParam(kAttack)->SetShape(2.);
+  GetParam(kRelease)->InitDouble("Release", 10, 1., 100.0, 0.1, "ms");
+  GetParam(kRelease)->SetShape(2.);
+  GetParam(kThreshold)->InitDouble("Threshold", 0., -40., 0.0, 0.1, "dB"); // threshold is actually power
+  GetParam(kThreshold)->SetShape(2.);
+  GetParam(kSlope)->InitDouble("Slope", 2., 1, 100, 1, "-");
+  GetParam(kSlope)->SetShape(2.);
+  GetParam(kSoftness)->InitDouble("Softness", -2, -4, 0, 0.1, "-");
+  GetParam(kSoftness)->SetShape(2.);
+  GetParam(kMakeup)->InitDouble("Makeup Gain", 0, 0, 40, 0.1, "-"); // Makeup is expressed in amplitude
+  GetParam(kMakeup)->SetShape(2.);
+  GetParam(kDryWet)->InitDouble("Dry/Wet", 1, 0, 1, 0.01, "-");
+  GetParam(kDryWet)->SetShape(1.);
+
+  IGraphics* pGraphics = MakeGraphics(this, kWidth, kHeight);
+  pGraphics->AttachBackground(COMPRESSOR_ID, COMPRESSOR_FN);
+
+  IBitmap knob = pGraphics->LoadIBitmap(KNOB_ID, KNOB_FN, kKnobFrames);
+  IBitmap knob1 = pGraphics->LoadIBitmap(KNOB1_ID, KNOB1_FN, kKnobFrames);
+  IText text = IText(10, 0, 0, IText::kStyleBold);
+  
+  pGraphics->AttachControl(new IKnobMultiControlText(this, IRECT(kAttackX, kAttackY, kAttackX + 43, kAttackY + 43 + 21), kAttack, &knob, &text, "ms"));
+  pGraphics->AttachControl(new IKnobMultiControlText(this, IRECT(kReleaseX, kReleaseY, kReleaseX + 43, kReleaseY + 43 + 21), kRelease, &knob, &text, "ms"));
+  pGraphics->AttachControl(new IKnobMultiControlText(this, IRECT(kThresholdX, kThresholdY, kThresholdX + 43, kThresholdY + 43 + 21), kThreshold, &knob, &text, "dB"));
+  pGraphics->AttachControl(new IKnobMultiControlText(this, IRECT(kSlopeX, kSlopeY, kSlopeX + 43, kSlopeY + 43 + 21), kSlope, &knob, &text, ""));
+  pGraphics->AttachControl(new IKnobMultiControl(this, kSoftnessX, kSoftnessY, kSoftness, &knob));
+  pGraphics->AttachControl(new IKnobMultiControlText(this, IRECT(kMakeupX, kMakeupY, kMakeupX + 43, kMakeupY + 43 + 21), kMakeup, &knob, &text, "dB"));
+  pGraphics->AttachControl(new IKnobMultiControl(this, kDryWetX, kDryWetY, kDryWet, &knob1));
+
+  AttachGraphics(pGraphics);
+
+  //MakePreset("preset 1", ... );
+  MakePreset("Serial Compression", 10., 10., 0., 2., -2., 0., 0.);
+  MakePreset("Parallel Compression", 10., 10., 0., 2., -2., 0., 0.5);
+  
+  powerFilter.set_input_port(0, &inFilter, 0);
+  gainCompressorFilter.set_input_port(0, &powerFilter, 0);
+  attackReleaseFilter.set_input_port(0, &gainCompressorFilter, 0);
+  applyGainFilter.set_input_port(0, &attackReleaseFilter, 0);
+  applyGainFilter.set_input_port(1, &inFilter, 0);
+  volumeFilter.set_input_port(0, &applyGainFilter, 0);
+  drywetFilter.set_input_port(0, &volumeFilter, 0);
+  drywetFilter.set_input_port(1, &inFilter, 0);
+  outFilter.set_input_port(0, &drywetFilter, 0);
+  
+  Reset();
+}
+
+ATKSideChainCompressor::~ATKSideChainCompressor() {}
+
+void ATKSideChainCompressor::ProcessDoubleReplacing(double** inputs, double** outputs, int nFrames)
+{
+  // Mutex is already locked for us.
+
+  inFilter.set_pointer(inputs[0], nFrames);
+  outFilter.set_pointer(outputs[0], nFrames);
+  outFilter.process(nFrames);
+}
+
+void ATKSideChainCompressor::Reset()
+{
+  TRACE;
+  IMutexLock lock(this);
+  
+  int sampling_rate = GetSampleRate();
+  
+  inFilter.set_input_sampling_rate(sampling_rate);
+  inFilter.set_output_sampling_rate(sampling_rate);
+  powerFilter.set_input_sampling_rate(sampling_rate);
+  powerFilter.set_output_sampling_rate(sampling_rate);
+  attackReleaseFilter.set_input_sampling_rate(sampling_rate);
+  attackReleaseFilter.set_output_sampling_rate(sampling_rate);
+  gainCompressorFilter.set_input_sampling_rate(sampling_rate);
+  gainCompressorFilter.set_output_sampling_rate(sampling_rate);
+  applyGainFilter.set_input_sampling_rate(sampling_rate);
+  applyGainFilter.set_output_sampling_rate(sampling_rate);
+  volumeFilter.set_input_sampling_rate(sampling_rate);
+  volumeFilter.set_output_sampling_rate(sampling_rate);
+  drywetFilter.set_input_sampling_rate(sampling_rate);
+  drywetFilter.set_output_sampling_rate(sampling_rate);
+  outFilter.set_input_sampling_rate(sampling_rate);
+  outFilter.set_output_sampling_rate(sampling_rate);
+
+  powerFilter.set_memory(std::exp(-1/1e-3 * GetSampleRate()));
+  attackReleaseFilter.set_release(std::exp(-1 / (GetParam(kAttack)->Value() * 1e-3 * GetSampleRate()))); // in ms
+  attackReleaseFilter.set_attack(std::exp(-1 / (GetParam(kRelease)->Value() * 1e-3 * GetSampleRate()))); // in ms
+}
+
+void ATKSideChainCompressor::OnParamChange(int paramIdx)
+{
+  IMutexLock lock(this);
+
+  switch (paramIdx)
+  {
+    case kThreshold:
+      gainCompressorFilter.set_threshold(std::pow(10, GetParam(kThreshold)->Value() / 10));
+      break;
+    case kSlope:
+      gainCompressorFilter.set_ratio(GetParam(kSlope)->Value());
+      break;
+    case kSoftness:
+      gainCompressorFilter.set_softness(std::pow(10, GetParam(kSoftness)->Value()));
+      break;
+    case kAttack:
+      attackReleaseFilter.set_release(std::exp(-1 / (GetParam(kAttack)->Value() * 1e-3 * GetSampleRate()))); // in ms
+      break;
+    case kRelease:
+      attackReleaseFilter.set_attack(std::exp(-1 / (GetParam(kRelease)->Value() * 1e-3 * GetSampleRate()))); // in ms
+      break;
+    case kMakeup:
+      volumeFilter.set_volume_db(GetParam(kMakeup)->Value());
+      break;
+    case kDryWet:
+      drywetFilter.set_dry(GetParam(kDryWet)->Value());
+      break;
+
+    default:
+      break;
+  }
+}
---
+++ ATKSideChainCompressor/resource.h
@@ -0,0 +1,88 @@
+#define PLUG_MFR "MatthieuBrucher"
+#define PLUG_NAME "ATKSideChainCompressor"
+
+#define PLUG_CLASS_NAME ATKSideChainCompressor
+
+#define BUNDLE_MFR "MatthieuBrucher"
+#define BUNDLE_NAME "ATKSideChainCompressor"
+
+#define PLUG_ENTRY ATKSideChainCompressor_Entry
+#define PLUG_VIEW_ENTRY ATKSideChainCompressor_ViewEntry
+
+#define PLUG_ENTRY_STR "ATKSideChainCompressor_Entry"
+#define PLUG_VIEW_ENTRY_STR "ATKSideChainCompressor_ViewEntry"
+
+#define VIEW_CLASS ATKSideChainCompressor_View
+#define VIEW_CLASS_STR "ATKSideChainCompressor_View"
+
+// Format        0xMAJR.MN.BG - in HEX! so version 10.1.5 would be 0x000A0105
+#define PLUG_VER 0x00010101
+#define VST3_VER_STR "1.1.1"
+
+// http://service.steinberg.de/databases/plugin.nsf/plugIn?openForm
+// 4 chars, single quotes. At least one capital letter
+#define PLUG_UNIQUE_ID '6m9f'
+// make sure this is not the same as BUNDLE_MFR
+#define PLUG_MFR_ID 'MatB'
+
+// ProTools stuff
+
+#if (defined(AAX_API) || defined(RTAS_API)) && !defined(_PIDS_)
+  #define _PIDS_
+  const int PLUG_TYPE_IDS[2] = {'EFN1', 'EFN2'};
+  const int PLUG_TYPE_IDS_AS[2] = {'EFA1', 'EFA2'}; // AudioSuite
+#endif
+
+#define PLUG_MFR_PT "MatthieuBrucher\nMatthieuBrucher\nMatthieuBrucher"
+#define PLUG_NAME_PT "ATKSideChainCompressor\n6m9f"
+#define PLUG_TYPE_PT "Dynamics"
+#define PLUG_DOES_AUDIOSUITE 1
+
+/* PLUG_TYPE_PT can be "None", "EQ", "Dynamics", "PitchShift", "Reverb", "Delay", "Modulation", 
+"Harmonic" "NoiseReduction" "Dither" "SoundField" "Effect" 
+instrument determined by PLUG _IS _INST
+*/
+
+#define PLUG_CHANNEL_IO "1-1"
+
+// Should be retrieved from ATK when the time comes
+#define PLUG_LATENCY 0
+#define PLUG_IS_INST 0
+
+// if this is 0 RTAS can't get tempo info
+#define PLUG_DOES_MIDI 0
+
+#define PLUG_DOES_STATE_CHUNKS 0
+
+// Unique IDs for each image resource.
+#define COMPRESSOR_ID 101
+#define KNOB_ID 102
+#define KNOB1_ID 103
+
+// Image resource locations for this plug.
+#define COMPRESSOR_FN "resources/img/Background.png"
+#define KNOB_FN "resources/img/KNB02uni43.png"
+#define KNOB1_FN "resources/img/KNB02bi43.png"
+
+// GUI default dimensions
+#define GUI_WIDTH 507
+#define GUI_HEIGHT 100
+
+// on MSVC, you must define SA_API in the resource editor preprocessor macros as well as the c++ ones
+#if defined(SA_API) && !defined(OS_IOS)
+#include "app_wrapper/app_resource.h"
+#endif
+
+// vst3 stuff
+#define MFR_URL "matt.eifelle.com"
+#define MFR_EMAIL "spam@me.com"
+#define EFFECT_TYPE_VST3 "Fx|Dynamics"
+
+/* "Fx|Analyzer"", "Fx|Delay", "Fx|Distortion", "Fx|Dynamics", "Fx|EQ", "Fx|Filter",
+"Fx", "Fx|Instrument", "Fx|InstrumentExternal", "Fx|Spatial", "Fx|Generator",
+"Fx|Mastering", "Fx|Modulation", "Fx|PitchShift", "Fx|Restoration", "Fx|Reverb",
+"Fx|Surround", "Fx|Tools", "Instrument", "Instrument|Drum", "Instrument|Sampler",
+"Instrument|Synth", "Instrument|Synth|Sampler", "Instrument|External", "Spatial",
+"Spatial|Fx", "OnlyRT", "OnlyOfflineProcess", "Mono", "Stereo",
+"Surround"
+*/
