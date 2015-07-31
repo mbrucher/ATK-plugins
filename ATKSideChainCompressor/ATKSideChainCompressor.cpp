7efdaa0d157cf2a4cf94a4555cdf599511177d9c
Matthieu Brucher <matthieu.brucher@gmail.Com>
2015-07-31 09:31:25 +0100

    Fix the power filters to be real Peak detectors.

--- ATKSideChainCompressor/ATKSideChainCompressor.cpp
+++ ATKSideChainCompressor/ATKSideChainCompressor.cpp
@@ -201,6 +201,9 @@ ATKSideChainCompressor::ATKSideChainCompressor(IPlugInstanceInfo instanceInfo)
   sumFilter.set_input_port(0, &powerFilter1, 0);
   sumFilter.set_input_port(1, &powerFilter2, 0);
 
+  powerFilter1.set_memory(0);
+  powerFilter2.set_memory(0);
+
   Reset();
 }
 
@@ -293,10 +296,8 @@ void ATKSideChainCompressor::Reset()
     endpoint.set_input_sampling_rate(sampling_rate);
     endpoint.set_output_sampling_rate(sampling_rate);
 
-    powerFilter1.set_memory(std::exp(-1 / 1e-3 * sampling_rate));
     attackReleaseFilter1.set_release(std::exp(-1 / (GetParam(kAttack1)->Value() * 1e-3 * sampling_rate))); // in ms
     attackReleaseFilter1.set_attack(std::exp(-1 / (GetParam(kRelease1)->Value() * 1e-3 * sampling_rate))); // in ms
-    powerFilter2.set_memory(std::exp(-1 / 1e-3 * GetSampleRate()));
     attackReleaseFilter2.set_release(std::exp(-1 / (GetParam(kAttack2)->Value() * 1e-3 * sampling_rate))); // in ms
     attackReleaseFilter2.set_attack(std::exp(-1 / (GetParam(kRelease2)->Value() * 1e-3 * sampling_rate))); // in ms
   }
