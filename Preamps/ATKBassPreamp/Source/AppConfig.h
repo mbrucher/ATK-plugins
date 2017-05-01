3d4eb887fbdae175fcf49b39431c61fc6b53e31e
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-05-01 11:49:27 +0100

    Changed the projects a little bit

--- Preamps/ATKBassPreamp/Source/AppConfig.h
+++ Preamps/ATKBassPreamp/Source/AppConfig.h
@@ -412,5 +412,5 @@
  #define JucePlugin_MaxNumOutputChannels   2
 #endif
 #ifndef  JucePlugin_PreferredChannelConfigurations
- #define JucePlugin_PreferredChannelConfigurations  {2,2}
+ #define JucePlugin_PreferredChannelConfigurations  {1,1}
 #endif
