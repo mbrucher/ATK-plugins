35d7f7d694aef603294432e19bf3d5e1a79b64a1
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-05-19 20:45:47 +0100

    Update the plugins to have automated parameters

--- Preamps/ATKBassPreamp/Source/PluginEditor.h
+++ Preamps/ATKBassPreamp/Source/PluginEditor.h
@@ -23,7 +23,7 @@
 class ATKBassPreampAudioProcessorEditor  : public AudioProcessorEditor
 {
 public:
-  ATKBassPreampAudioProcessorEditor (ATKBassPreampAudioProcessor&);
+  ATKBassPreampAudioProcessorEditor(ATKBassPreampAudioProcessor& p, AudioProcessorValueTreeState& paramState);
   ~ATKBassPreampAudioProcessorEditor();
 
   //==============================================================================
@@ -34,6 +34,7 @@ private:
   // This reference is provided as a quick way for your editor to
   // access the processor object that created it.
   ATKBassPreampAudioProcessor& processor;
+  AudioProcessorValueTreeState& paramState;
   
   ATK::juce::VolumeFilterComponent gain;
   ATK::juce::ToneStackFilterComponent stack;
